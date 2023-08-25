893050f72a59b27300b135f03fd0b99f7dd290a0
Edson Santos <edsondhonatadossantos@gmail.com>
2023-08-24 23:25:57 -0300

    Add project files.

---
+++ Hosp.Corporative/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hosp.Corporative.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
