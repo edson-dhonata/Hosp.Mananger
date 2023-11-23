e6f8ab975a605d4ecf2b81a10c81eed2dd994cf2
Edson Santos <edsondhonatadossantos@gmail.com>
2023-11-22 23:35:46 -0300

    Configurações globais no contexto e mapeamento de dados

--- HospMananger.Data/Data/ORM/ApplicationDbContext.cs
+++ HospMananger.Data/Data/ORM/ApplicationDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using HospManager.Domain.Entities;
+using HospMananger.Data.Mappings;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HospMananger.Data.Data.ORM
 {
@@ -17,6 +13,34 @@ namespace HospMananger.Data.Data.ORM
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            //Pega todas as proprierdades de modelo cujo o tipo seja string
+            var listaPropriedades = modelBuilder.Model.GetEntityTypes()
+                                                      .SelectMany(x => x.GetProperties().Where(x => x.ClrType == typeof(string)));
+
+
+            //Percorre essas propriedades
+            foreach (var property in listaPropriedades)
+            {
+                //Define elas para varchar 90 ao invez delas ficarem com tamanho varchar(max) que vem como padrão.
+                property.SetColumnType("varchar(90)");
+            }
+
+            //Aplica configurações feitas nas classes mapeadas de forma manual.
+            //modelBuilder.ApplyConfiguration(new EstadoPacienteMap());
+            //modelBuilder.ApplyConfiguration(new PacienteMap());
+
+            //Aplica configuração de mapeamento para todas as entidades que fazem parte do contexto via assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            //Pega todos os relacionamentos dos modelos.
+            var listaRelacionamentos = modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys());
+
+            foreach (var relacionamento in listaRelacionamentos)
+            {
+                relacionamento.DeleteBehavior = DeleteBehavior.ClientSetNull; // Seta nulo para cascata quando deletar elementos relacionados.
+            }
+
             base.OnModelCreating(modelBuilder); 
         }
     }
---
+++ HospMananger.Data/Migrations/20231123023042_AdicionaMapeamento.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospMananger.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaMapeamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Paciente_EstadoPaciente_EstadoPacienteId",
+                table: "Paciente");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RgOrgao",
+                table: "Paciente",
+                type: "varchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "Paciente",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Paciente",
+                type: "varchar(80)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Motivo",
+                table: "Paciente",
+                type: "varchar(90)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Paciente",
+                type: "varchar(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Paciente",
+                type: "varchar(11)",
+                fixedLength: true,
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "EstadoPaciente",
+                type: "varchar(30)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Paciente_EstadoPaciente_EstadoPacienteId",
+                table: "Paciente",
+                column: "EstadoPacienteId",
+                principalTable: "EstadoPaciente",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Paciente_EstadoPaciente_EstadoPacienteId",
+                table: "Paciente");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RgOrgao",
+                table: "Paciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rg",
+                table: "Paciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Paciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Motivo",
+                table: "Paciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(90)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Paciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "CPF",
+                table: "Paciente",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldFixedLength: true,
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "EstadoPaciente",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Paciente_EstadoPaciente_EstadoPacienteId",
+                table: "Paciente",
+                column: "EstadoPacienteId",
+                principalTable: "EstadoPaciente",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
